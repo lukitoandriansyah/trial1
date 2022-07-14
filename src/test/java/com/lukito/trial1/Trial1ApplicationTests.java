df9924c6cc98bbfa0605b063f619474307c4b003
lukitoandriansyah <lukitoandriansyah45@gmail.com>
2022-07-14 15:22:18 +0700

    initial commit

---
+++ src/test/java/com/lukito/trial1/Trial1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lukito.trial1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Trial1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
